97253b724185376eb11d8ae571862b05dd912da0
brian432 <brian_cabrera123@outlook.es>
2023-06-30 19:15:40 -0300

    variables de entorno modificadas

--- client/next.config.js
+++ client/next.config.js
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /*env: {
-    API_KEY: 'api_key=04c35731a5ee918f014970082a0088b1',
-    API_TMDB: 'https://api.themoviedb.org/3/discover/',
-    API_DETAILS: 'https://api.themoviedb.org/3/',
-    GENRE_API: 'https://api.themoviedb.org/3/genre/',
-    API_IMAGE: 'https://image.tmdb.org/t/p/w500',
-    API_IMAGE_BACKGROUND: 'https://image.tmdb.org/t/p/original',
-    API_SERVER: 'http://localhost:3001'
-  },*/
   images: {
     domains: ['image.tmdb.org'],
   }
