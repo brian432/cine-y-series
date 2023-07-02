d07100993c90f40df88b5a5a87e2a6088398e9e7
brian432 <brian_cabrera123@outlook.es>
2023-07-02 00:27:38 -0300

    .

--- server/src/routes/login.ts
+++ server/src/routes/login.ts
@@ -31,7 +31,6 @@ loginRouter.post('/', validateLogin, async (req: Request, res: Response): Promis
     res.cookie('token', token, {
       maxAge: 24 * 60 * 60 * 1000,
       secure: true,
-      domain: 'https://cine-y-series.vercel.app/',
       sameSite: 'none'
     })
 
